4760b69955b37dcea0e88da3b9852dad623c5dd4
NewEgoDocs <juhan211@naver.com>
2023-12-18 23:09:04 +0900

    initial commit

---
+++ src/main/kotlin/elastic/search/connect/elasticsearch/ElasticsearchApplication.kt
@@ -0,0 +1,11 @@
+package elastic.search.connect.elasticsearch
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ElasticsearchApplication
+
+fun main(args: Array<String>) {
+    runApplication<ElasticsearchApplication>(*args)
+}
